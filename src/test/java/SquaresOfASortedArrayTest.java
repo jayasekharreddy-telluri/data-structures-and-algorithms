6b6c2f7d993ed1f309a214af58cf07cc3e2f7158
jai <varshitjai@gmail.com>
2023-07-02 16:04:30 +0530

    added SquaresOfASortedArray problem and j-unit test cases

---
+++ src/test/java/SquaresOfASortedArrayTest.java
@@ -0,0 +1,26 @@
+
+
+import org.jai.pattrens.twopointers.SquaresOfASortedArray;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArrayTest {
+
+    @Test
+    public void testBruteForceSolution() {
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] expected = {0, 1, 9, 16, 100};
+        int[] result = SquaresOfASortedArray.bruteForceSolution(nums);
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testOptimalSolution() {
+        int[] nums = {0};
+        int[] expected = {0};
+        int[] result = SquaresOfASortedArray.optimalSolution(nums);
+        Assert.assertArrayEquals(expected, result);
+    }
+}
