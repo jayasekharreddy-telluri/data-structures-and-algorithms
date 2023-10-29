526ebffa443267f1004e96e9bf7695e185b08e02
jai <varshitjai@gmail.com>
2023-10-29 12:46:44 +0530

    added two new problemes to package -binarysearch

---
+++ src/main/java/org/com/jai/pattrens/binarysearch/NextLetter.java
@@ -0,0 +1,39 @@
+package org.com.jai.pattrens.binarysearch;
+
+public class NextLetter {
+
+    public static char searchNextLetter(char[] letters, char key) {
+        if (key < letters[0] || key > letters[letters.length - 1]) {
+            return letters[0]; // If the key is smaller than the smallest letter or greater than the largest letter, return the smallest letter.
+        }
+
+        int start = 0; // Initialize the start index.
+        int end = letters.length - 1; // Initialize the end index.
+
+        while (start <= end) { // While the search range is valid.
+            int mid = start + (end - start) / 2; // Calculate the middle index.
+
+            if (key < letters[mid]) {
+                end = mid - 1; // If the key is smaller, search the left half.
+            } else {
+                start = mid + 1; // If the key is greater or equal, search the right half.
+            }
+        }
+        // To account for the wrap-around in the circular array, use modulo.
+        return letters[start % letters.length];
+
+
+        /*
+
+                Time Complexity: O(log N)
+                Space Complexity: O(1)
+
+         */
+    }
+
+    public static void main(String[] args) {
+        char[] letters = {'a', 'c', 'f', 'h'};
+        char key = 'f';
+        System.out.println(searchNextLetter(letters, key));
+    }
+}
---
+++ src/test/java/binarysearch/NumberRange.java
@@ -0,0 +1,62 @@
+package binarysearch;
+
+import java.util.Arrays;
+
+public class NumberRange {
+
+    // Function to find the range of a given key in a sorted array.
+    public static int[] findRange(int[] nums, int key) {
+        int[] result = new int[]{-1, -1}; // Initialize the result array with default values.
+
+        // Find the leftmost occurrence of the key in the array.
+        result[0] = search(nums, key, false);
+
+        if (result[0] != -1) {
+            // If the key was found, find the rightmost occurrence of the key in the array.
+            result[1] = search(nums, key, true);
+        }
+
+        return result; // Return the range of the key (leftmost and rightmost indices).
+    }
+
+    // Helper function to perform binary search for finding the leftmost or rightmost occurrence of a key.
+    private static int search(int[] nums, int key, boolean findMaxRange) {
+        int keyIndex = -1; // Initialize the index of the key.
+
+        int start = 0; // Initialize the start index of the search range.
+        int end = nums.length - 1; // Initialize the end index of the search range.
+
+        while (start <= end) { // While the search range is valid.
+            int mid = start + (end - start) / 2; // Calculate the middle index.
+
+            if (key < nums[mid]) {
+                end = mid - 1; // If the key is smaller, search the left half of the array.
+            } else if (key > nums[mid]) {
+                start = mid + 1; // If the key is larger, search the right half of the array.
+            } else {
+                keyIndex = mid; // If the key is found, update the key index.
+
+                if (findMaxRange) {
+                    start = mid + 1; // If finding the rightmost occurrence, move to the right of the mid.
+                } else {
+                    end = mid - 1; // If finding the leftmost occurrence, move to the left of the mid.
+                }
+            }
+        }
+
+        return keyIndex; // Return the index of the key (leftmost or rightmost occurrence).
+
+        /*
+
+                Time Complexity: O(log N)
+                Space Complexity: O(1)
+
+         */
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 6, 6, 6, 9};
+        int key = 6;
+        System.out.println(Arrays.toString(findRange(nums, key))); // Print the range of the key.
+    }
+}
