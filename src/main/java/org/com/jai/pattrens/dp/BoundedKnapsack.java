15f9c4b0fd35f8fde0463e77473895d32d4b79d4
jai <varshitjai@gmail.com>
2023-11-29 21:40:36 +0530

    added two problemes to dp package

--- src/main/java/org/com/jai/pattrens/dp/BoundedKnapsack.java
+++ src/main/java/org/com/jai/pattrens/dp/BoundedKnapsack.java
@@ -2,50 +2,34 @@ package org.com.jai.pattrens.dp;
 
 public class BoundedKnapsack {
 
-    public static int getMaxProfit(int[] weight,int[] profit,int capacity){
 
-        if (weight.length == 0 || profit.length == 0 || capacity <= 0){
-
+    public static int getMaxProfit(int[] weight, int[] profit, int capacity, int currentIndex) {
+        // Base Case
+        if (currentIndex < 0 || capacity <= 0) {
             return 0;
         }
 
-        int[][] arr = new int[weight.length][capacity + 1];
-
-        for (int j = 0; j <= capacity; j++){
-
-            if (weight[0] <= j){
-
-                arr[0][j] = profit[0];
-
-            }
+        // If the current item's weight exceeds the capacity, skip it
+        if (weight[currentIndex] > capacity) {
+            return getMaxProfit(weight, profit, capacity, currentIndex - 1);
         }
 
-        for (int i = 1; i < weight.length; i++){
+        // Recursive steps:
+        // Choose between including or excluding the current item
+        int includeProfit = profit[currentIndex] +
+                getMaxProfit(weight, profit, capacity - weight[currentIndex], currentIndex - 1);
+        int excludeProfit = getMaxProfit(weight, profit, capacity, currentIndex - 1);
 
-            for (int j = 1; j <= capacity; j++){
-
-                int includingProfit = 0;
-
-                if (weight[i] <= j){
-                    includingProfit = profit[i] + arr[i -1][j - weight[i]];
-                }
-
-                arr[i][j] = Math.max(includingProfit, arr[i -1 ][j]);
-            }
-        }
-
-        return arr[weight.length - 1][capacity];
-
+        // Return the maximum profit obtained
+        return Math.max(includeProfit, excludeProfit);
     }
 
     public static void main(String[] args) {
-
         int[] wt = {1, 2, 3, 5};
         int[] pr = {1, 4, 7, 10};
-
         int capacity = 8;
+        int currentIndex = wt.length - 1; // Start from the last item
 
-        System.out.println(BoundedKnapsack.getMaxProfit(wt, pr, capacity));
-
+        System.out.println(getMaxProfit(wt, pr, capacity, currentIndex));
     }
 }
